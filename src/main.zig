ac6b38ff8717737e531118150941c3f5672b0c6c
hans <hansolivas1@gmail.com>
2024-12-28 22:30:54 -0500

    feat: done.

--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,37 @@
 const std = @import("std");
 
 pub fn main() !void {
-    std.debug.print("Hello", .{});
+    const allocator = std.heap.page_allocator;
+    const args = std.process.argsAlloc(allocator) catch |err| {
+        std.log.err("Failed to alloc arguments: {s}", .{@errorName(err)});
+        return;
+    };
+    defer std.process.argsFree(allocator, args);
+
+    if (args.len < 3) {
+        std.log.info("{s} <file> <pattern>", .{args[0]});
+        return;
+    }
+
+    const file_name = args[1];
+    const to_match = args[2];
+
+    const file: std.fs.File = std.fs.cwd().openFile(file_name, .{ .mode = .read_only }) catch |err| {
+        std.log.err("Failed to open file '{s}': {s}", .{ file_name, @errorName(err) });
+        return;
+    };
+    defer file.close();
+
+    var buffer: [4096]u8 = undefined;
+    while (file.reader().readUntilDelimiterOrEof(&buffer, '\n') catch |err| {
+        std.log.err("Failed to read from file: {s}", .{@errorName(err)});
+        return;
+    }) |line| {
+        const found = std.mem.indexOf(u8, line, to_match);
+        if (found != null) {
+            std.debug.print("{s}\n", .{line});
+        }
+    }
 }
 
 test "simple test" {
